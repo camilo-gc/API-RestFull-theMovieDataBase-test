d6fd4e3b29f90333f916382977665ebb581d1325
camilo-gc <camilogarcia.dev@gmail.com>
2023-04-04 14:56:35 -0500

    first commit

---
+++ src/main/java/com/talataa/apirestfull/RestFullApiTheMovieDbTestApplication.java
@@ -0,0 +1,13 @@
+package com.talataa.apirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullApiTheMovieDbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFullApiTheMovieDbTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talataa/apirestfull/RestFullApiTheMovieDbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talataa.apirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullApiTheMovieDbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
